7972d584c083e7b780180807fe816aa58be6396d
Butch Cortel <cortel.butch.kleina@gmail.com>
2020-12-25 22:46:45 +0800

    Added projects

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,18 @@
 const showContent = (target) => {
-    const {id} = target;
-    const lis = document.getElementsByClassName("aboutLis");
-    const elements = document.getElementsByClassName("aboutDivs");
-    for (let i = 0; i < elements.length; i++) {
-        
-        const element = elements[i];
-        const li = lis[i];
-        if (id == i) {
-            li.style.borderBottom = "3px orangered solid"
-            li.style.color = "orangered"
-            element.style.display = "block"
-            
-        }
-        else {
-            li.style.color = "white"
-            li.style.borderBottom = "none"
-            element.style.display = "none"
-        }
-        
+  const { id } = target;
+  const lis = document.getElementsByClassName("aboutLis");
+  const elements = document.getElementsByClassName("aboutDivs");
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const li = lis[i];
+    if (id == i) {
+      li.style.borderBottom = "3px orangered solid";
+      li.style.color = "orangered";
+      element.style.display = "block";
+    } else {
+      li.style.color = "white";
+      li.style.borderBottom = "none";
+      element.style.display = "none";
     }
-}
+  }
+};
